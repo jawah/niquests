2a4397dfc902871be7c5d95d6e1f0d2e0b090bb8
TAHRI Ahmed R <Ousret@users.noreply.github.com>
2024-07-07 23:30:57 +0100

    Release 3.7.1 (#135)
    3.7.1 (2024-07-07)
    ------------------
    
    **Added**
    - Official support for Python 3.13
    - Support for asynchronous auth callables.
    - Support for asynchronous bodies through `AsyncIterable` that yield
    either bytes or str.
    - Support for purposely excluding a domain/port from connecting to
    QUIC/HTTP3 via the `quic_cache_layer` property of `Session`.
      In order to exclude `cloudflare.com` from HTTP3 auto-upgrade:
      ```python
      from niquests import Session
    
      s = Session()
      s.quic_cache_layer.exclude_domain("cloudflare.com")
      ```
    
    **Fixed**
    - auth argument not accepting a function according to static type
    checkers. (#133)
    - RequestsCookieJar having a lock in `AsyncSession`. Its effect has been
    nullified to improve performances.
    
    **Changed**
    - urllib3-future lower bound version is raised to 2.8.902
    
    ---------
    
    Co-authored-by: MtkN1 <51289448+MtkN1@users.noreply.github.com>

--- src/niquests/__version__.py
+++ src/niquests/__version__.py
@@ -9,9 +9,9 @@ __description__: str = "Python HTTP for Humans."
 __url__: str = "https://niquests.readthedocs.io"
 
 __version__: str
-__version__ = "3.7.0"
+__version__ = "3.7.1"
 
-__build__: int = 0x030700
+__build__: int = 0x030701
 __author__: str = "Kenneth Reitz"
 __author_email__: str = "me@kennethreitz.org"
 __license__: str = "Apache-2.0"
