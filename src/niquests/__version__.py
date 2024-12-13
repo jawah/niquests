0965750eeb6aa90361503de53927b214e012b4ed
Ahmed TAHRI <ahmed.tahri@cloudnursery.dev>
2024-12-13 07:41:15 +0100

    :bookmark: bump version to 3.11.3

--- src/niquests/__version__.py
+++ src/niquests/__version__.py
@@ -9,9 +9,9 @@ __description__: str = "Python HTTP for Humans."
 __url__: str = "https://niquests.readthedocs.io"
 
 __version__: str
-__version__ = "3.11.2"
+__version__ = "3.11.3"
 
-__build__: int = 0x031102
+__build__: int = 0x031103
 __author__: str = "Kenneth Reitz"
 __author_email__: str = "me@kennethreitz.org"
 __license__: str = "Apache-2.0"
