01fe86ee0bab277da7e307ca7e992a42af68ac30
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-02-06 16:34:18 +0100

    :bookmark: bump version to 3.13

--- src/niquests/__version__.py
+++ src/niquests/__version__.py
@@ -9,9 +9,9 @@ __description__: str = "Python HTTP for Humans."
 __url__: str = "https://niquests.readthedocs.io"
 
 __version__: str
-__version__ = "3.12.3"
+__version__ = "3.13.0"
 
-__build__: int = 0x031203
+__build__: int = 0x031300
 __author__: str = "Kenneth Reitz"
 __author_email__: str = "me@kennethreitz.org"
 __license__: str = "Apache-2.0"
