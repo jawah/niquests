5ddb7d7d6a9bf5cea08dce9d91378fec218cf1f8
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-03-30 18:37:00 +0200

    :wrench: ensure _async module still exist (backward compatibility)

---
+++ src/niquests/_async.py
@@ -0,0 +1,12 @@
+import warnings
+
+warnings.warn(
+    (
+    "importing niquests._async is deprecated and absolutely discouraged. "
+    "It will be removed in a future release. In general, never import private "
+    "modules."
+    ),
+    DeprecationWarning,
+)
+
+from .async_session import *  # noqa
