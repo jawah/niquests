a9562332fc1e69128ed7d32a4ae6541e42a14e7e
UnknownApi <matthias.avicii@gmail.com>
2025-04-22 19:56:32 +0200

    Fixing circular import in middlewares.py, added unit test + documentation.

--- src/niquests/middlewares.py
+++ src/niquests/middlewares.py
@@ -1,13 +1,16 @@
+import typing
 from inspect import iscoroutinefunction
-from typing import Optional, Callable, Any, TypeVar, Iterable
-from . import PreparedRequest, Response
+from typing import Iterable
 from abc import ABC
+
+if typing.TYPE_CHECKING:
+    from .models import PreparedRequest, Response
 
 
 class Middleware(ABC):
     """Base class for synchronous request lifecycle middlewares."""
 
-    def on_request(self, request: PreparedRequest,*args, **kwargs) -> None:
+    def on_request(self, request: 'PreparedRequest', *args, **kwargs) -> None:
         """Called after the request is prepared but before it’s sent.
 
         :param request: The prepared request object.
@@ -15,7 +18,7 @@ class Middleware(ABC):
         """
         pass
 
-    def on_response(self, response: Response,*args, **kwargs) -> None:
+    def on_response(self, response: 'Response', *args, **kwargs) -> None:
         """Called after receiving a response.
 
         :param response: The response object.
@@ -35,25 +38,24 @@ class MiddlewareExecutor:
     def __init__(self, middlewares: Iterable[Middleware]):
         self.middlewares = middlewares
 
-    def execute_on_request_sync(self, request: PreparedRequest, *args, **kwargs) -> None:
+    def execute_on_request_sync(self, request: 'PreparedRequest', *args, **kwargs) -> None:
         for middleware in self.middlewares:
-            middleware.on_request(request,*args, **kwargs) or request
+            middleware.on_request(request, *args, **kwargs) or request
 
-    def execute_on_response_sync(self, response: Response,*args, **kwargs) -> None:
+    def execute_on_response_sync(self, response: 'Response', *args, **kwargs) -> None:
         for middleware in self.middlewares:
-            middleware.on_response(response,*args, **kwargs) or response
+            middleware.on_response(response, *args, **kwargs) or response
 
-    async def execute_on_request_async(self, request: PreparedRequest, *args, **kwargs) -> None:
+    async def execute_on_request_async(self, request: 'PreparedRequest', *args, **kwargs) -> None:
         for middleware in self.middlewares:
             if iscoroutinefunction(middleware.on_request):
-                await middleware.on_request(request=request,*args, **kwargs)
+                await middleware.on_request(request=request, *args, **kwargs)
             else:
-                middleware.on_request(request=request,*args, **kwargs)
+                middleware.on_request(request=request, *args, **kwargs)
 
-    async def execute_on_response_async(self, response: Response, *args, **kwargs) -> None:
+    async def execute_on_response_async(self, response: 'Response', *args, **kwargs) -> None:
         for middleware in self.middlewares:
             if iscoroutinefunction(middleware.on_response):
-                await middleware.on_response(response=response,*args, **kwargs)
+                await middleware.on_response(response=response, *args, **kwargs)
             else:
-                middleware.on_response(response=response, *args,**kwargs)
-
+                middleware.on_response(response=response, *args, **kwargs)
