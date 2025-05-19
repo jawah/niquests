87944924c64c9a6780d5d32dc9b677f53897135c
UnknownApi <matthias.avicii@gmail.com>
2025-05-19 21:42:35 +0200

    Added args kwargs to middleware methods argument, improves extensibility

--- src/niquests/middlewares.py
+++ src/niquests/middlewares.py
@@ -5,7 +5,7 @@ Middlewares implement a way of intercepting requests at different points in the 
 from __future__ import annotations
 
 from abc import ABC
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from .models import PreparedRequest, Response
@@ -14,48 +14,48 @@ if TYPE_CHECKING:
 class Middleware(ABC):
     """Base class for synchronous middlewares."""
 
-    def pre_request(self, request: PreparedRequest) -> None:
+    def pre_request(self, request: PreparedRequest, *args: Any, **kwargs: Any) -> None:
         """Called before the request is sent."""
 
-    def pre_send(self, request: PreparedRequest) -> None:
+    def pre_send(self, request: PreparedRequest, *args: Any, **kwargs: Any) -> None:
         """The prepared request got his ConnectionInfo injected.
         This event is triggered just after picking a live connection from the pool"""
 
     def on_upload(
-        self, request: PreparedRequest
+        self, request: PreparedRequest, *args: Any, **kwargs: Any
     ) -> None:  # consider passing the block of data in question as a parameter. (requires changes in urllib3.future)
         """Permit to monitor the upload progress of passed body.
         This event is triggered each time a block of data is transmitted to the remote peer.
         Use this hook carefully as it may impact the overall performance."""
 
-    def early_response(self, response: Response) -> None:
+    def early_response(self, response: Response, *args: Any, **kwargs: Any) -> None:
         """An early response caught before receiving the final Response for a given Request. Like but not limited to 103 Early Hints.
         This event is triggered before the Response is returned to the user."""
 
-    def response(self, response: Response) -> None:
+    def response(self, response: Response, *args: Any, **kwargs: Any) -> None:
         """Called when a response is received."""
 
 
 class AsyncMiddleware(ABC):
     """Base class for asynchronous middlewares."""
 
-    async def pre_request(self, request: PreparedRequest) -> None:
+    async def pre_request(self, request: PreparedRequest, *args: Any, **kwargs: Any) -> None:
         """Called before the request is sent."""
 
-    async def pre_send(self, request: PreparedRequest) -> None:
+    async def pre_send(self, request: PreparedRequest, *args: Any, **kwargs: Any) -> None:
         """The prepared request got his ConnectionInfo injected.
         This event is triggered just after picking a live connection from the pool"""
 
-    async def on_upload(self, request: PreparedRequest) -> None:
+    async def on_upload(self, request: PreparedRequest, *args: Any, **kwargs: Any) -> None:
         """Permit to monitor the upload progress of passed body.
         This event is triggered each time a block of data is transmitted to the remote peer.
         Use this hook carefully as it may impact the overall performance."""
 
-    async def early_response(self, response: Response) -> None:
+    async def early_response(self, response: Response, *args: Any, **kwargs: Any) -> None:
         """An early response caught before receiving the final Response for a given Request. Like but not limited to 103 Early Hints.
         This event is triggered before the Response is returned to the user."""
 
-    async def response(self, response: Response) -> None:
+    async def response(self, response: Response, *args: Any, **kwargs: Any) -> None:
         """Called when a response is received."""
 
 
