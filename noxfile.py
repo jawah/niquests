76436b67d4eef232acebf1185609bec8460ffa45
Ahmed TAHRI <tahri.ahmed@proton.me>
2026-07-29 08:33:51 +0100

    chore: add nox session py3.15

--- noxfile.py
+++ noxfile.py
@@ -82,7 +82,7 @@ def tests_impl(
     )
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14", "pypy3.11"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14", "3.15", "pypy3.11"])
 def test(session: nox.Session) -> None:
     tests_impl(session)
 
